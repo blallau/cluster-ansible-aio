3aa5c4f84df2084b20bd204d16ec21c875c322a6
Bertrand Lallau <bertrand.lallau@gmail.com>
2018-11-12 20:42:05 +0100

    Fix cluster.sh using gracefully_shutdown

--- cluster.sh
+++ cluster.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-#set -x
 
-NODE_PREFIX="okd-"
+NODE_PREFIX=""
 SERVICES="${NODE_PREFIX}controller-1 ${NODE_PREFIX}node-1 ${NODE_PREFIX}node-2"
 SNAPSHOT_NAME=""
+TIMEOUT=300
 
 function start {
+    docker start docker-dnsmasq
     for service in $SERVICES; do
         virsh start $service
     done
@@ -23,14 +24,40 @@ function undefine {
     done
 }
 
-function shutdown {
-    for service in $SERVICES; do
-        virsh shutdown $service
-    done
+function list_running_domains() {
+    virsh list | grep "running" | awk '{ print $2}'
+}
+
+function gracefully_shutdown {
+	echo "Try to cleanly shut down all running KVM domains..."
+
+	# Try to shutdown each domain, one by one.
+	list_running_domains | while read DOMAIN; do
+		# Try to shutdown given domain.
+		virsh shutdown $DOMAIN --mode acpi
+	done
+
+	# Wait until all domains are shut down or timeout has reached.
+	END_TIME=$(date -d "$TIMEOUT seconds" +%s)
+
+	while [ $(date +%s) -lt $END_TIME ]; do
+		# Break while loop when no domains are left.
+		test -z "$(list_running_domains)" && break
+		# Wait a litte, we don't want to DoS libvirt.
+		sleep 1
+	done
+
+	# Clean up left over domains, one by one.
+	list_running_domains | while read DOMAIN; do
+		# Try to shutdown given domain.
+		virsh destroy $DOMAIN
+		# Give libvirt some time for killing off the domain.
+		sleep 3
+	done
 }
 
 function snap-create {
-    shutdown
+    gracefully_shutdown
     for service in $SERVICES; do
         virsh snapshot-create-as --domain $service --name $SNAPSHOT_NAME
         echo "Domain $service snapshoted"
@@ -88,7 +115,7 @@ while [ "$#" -gt 0 ]; do
         exit 0
         ;;
     (--shutdown)
-        shutdown
+        gracefully_shutdown
         exit 0
         ;;
     (--snap-delete)
